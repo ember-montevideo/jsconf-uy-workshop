a1a56d804be3670e21b87669afba64a69797d2cf
Adrian Mugnolo <adrian@mugnolo.com>
2016-06-08 17:55:26 -0300

    Add checks for FastBoot

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,14 @@
 import RESTAdapter from 'ember-data/adapters/rest';
+import Ember from 'ember';
 
 export default RESTAdapter.extend({
   namespace: 'api',
-  host: 'http://em-ng-workshop.herokuapp.com'
+
+  fastboot: Ember.inject.service(),
+
+  host: Ember.computed(function() {
+    if (this.get('fastboot.isFastBoot')) {
+      return 'http://em-ng-workshop.herokuapp.com';
+    }
+  })
 });
--- app/routes/album.js
+++ app/routes/album.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  fastboot: Ember.inject.service(),
+
   afterModel(album) {
-    return album.get('tracks');
+    if (this.get('fastboot.isFastBoot')) {
+      return album.get('tracks');
+    }
   }
 });
